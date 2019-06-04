ce9da90abd647c15c109a41a5e57eb5c2198654a
didierdewasseige <didierdewasseige@gmail.com>
2019-06-04 17:58:48 +0200

    controller estimates creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'estimates/index'
+  get 'estimates/new'
+  get 'estimates/create'
+  get 'estimates/show'
+  get 'estimates/edit'
+  get 'estimates/update'
+  get 'estimates/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
