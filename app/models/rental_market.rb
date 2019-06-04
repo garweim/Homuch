e34da9399935aef1a57427bd96a6c0c855246f53
gary weimberg <gary1992@hotmail.com>
2019-06-04 17:44:08 +0200

    rectification migrations and models

--- app/models/rental_market.rb
+++ app/models/rental_market.rb
@@ -1,2 +1,7 @@
 class RentalMarket < ApplicationRecord
+  validates :address, presence: true
+  validates :surface, presence: true
+  validates :nr_of_bedrooms, presence: true
+  validates :nr_of_bathrooms, presence: true
+  validates :price, presence: true
 end
--- app/models/sell_market.rb
+++ app/models/sell_market.rb
@@ -1,2 +1,8 @@
 class SellMarket < ApplicationRecord
+  validates :address, presence: true
+  validates :surface, presence: true
+  validates :nr_of_bedrooms, presence: true
+  validates :nr_of_bathrooms, presence: true
+  validates :price, presence: true
+
 end
