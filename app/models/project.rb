4b4457e871ba159731d5443029f9220be37ab2f8
aida bennouna <aida.bennouna89@gmail.com>
2019-06-05 15:24:38 +0200

    login condition sending to different forms

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,7 +6,7 @@ class Project < ApplicationRecord
   has_many :estimates, dependent: :destroy
   validates :name, :zipcode, :surface, :nr_of_bedrooms,
             :category, :nr_of_bathrooms, :state, :garage,
-            :heating, :electricity, :kitchen, :sanitation,
             presence: true
   validates :state, inclusion: { in: STATE }
+  validates :heating, :electricity, :kitchen, :sanitation, inclusion: { in: [true, false] }
 end
