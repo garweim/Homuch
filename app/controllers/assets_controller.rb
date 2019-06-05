a3318c608a6cbeb18e3bd7fd88987853f5b24dc2
aida bennouna <aida.bennouna89@gmail.com>
2019-06-05 14:03:28 +0200

    ok

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,6 +1,6 @@
 class AssetsController < ApplicationController
-  # before_action :find_id, only: [:show, :edit, :update, :destroy]
-  # skip_before_action :authenticate_user!, only: :index
+   before_action :find_id, only: [:show, :edit, :update, :destroy]
+   skip_before_action :authenticate_user!, only: :index, :new
   def index
     @assets = Asset.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  resources 'assets' do
   resources 'estimates'
+  end
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
