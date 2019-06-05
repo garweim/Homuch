2fc7f582c34001ebc9790a94bee74fb91f7de235
didierdewasseige <didierdewasseige@gmail.com>
2019-06-05 15:48:54 +0200

    detailed calculation

--- app/service/calculation.rb
+++ app/service/calculation.rb
@@ -1,6 +1,6 @@
 class Calculation
-  def market_price(zip_code, sqm, bed, bath)
-    filtered_array = SellMarket.where(zip_code: zip_code, surface: sqm-20 ..sqm+20, nr_of_bedrooms: bed, nr_of_bathrooms: bath)
+  def market_price(zipcode, sqm, bed, bath)
+    filtered_array = SellMarket.where(zipcode: zipcode, surface: sqm-20 ..sqm+20, nr_of_bedrooms: bed, nr_of_bathrooms: bath)
 
     price_per_sqm = []
     filtered_array.each do |x|
@@ -15,6 +15,4 @@ class Calculation
     sum/filtered_array.count
   end
 end
-
-puts market_price(1000, 200, 3, 2)
 
