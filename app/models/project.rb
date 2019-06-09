b82a5c2b306d47ea91981155dc913d5526caeb7c
gary weimberg <gary1992@hotmail.com>
2019-06-09 18:32:45 +0200

    pics and scraper

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,12 +6,12 @@ class Project < ApplicationRecord
   has_many :renovations, dependent: :destroy
   has_many :estimates, dependent: :destroy
 
-  # validates :name, :zipcode, :surface, :nr_of_bedrooms,
-  #           :category, :nr_of_bathrooms, :state, :garage,
-  #           presence: true
+  validates :name, :zipcode, :surface, :nr_of_bedrooms,
+            :category, :nr_of_bathrooms, :state, :garage,
+            presence: true
 
-  # validates :state, inclusion: { in: STATE }
-  # validates :heating, :electricity, :kitchen, :sanitation, inclusion: { in: [true, false] }
+  validates :state, inclusion: { in: STATE }
+  validates :heating, :electricity, :kitchen, :sanitation, inclusion: { in: [true, false] }
 
   geocoded_by :street_and_nr
   after_validation :geocode, if: :will_save_change_to_street_and_nr?
