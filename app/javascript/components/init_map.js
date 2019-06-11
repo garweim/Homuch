e3d9c5618e7434acf46e94902cf4e1ff52910068
gary weimberg <gary1992@hotmail.com>
2019-06-11 14:22:25 +0200

    something

--- app/javascript/components/init_map.js
+++ app/javascript/components/init_map.js
@@ -12,24 +12,17 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken }));
 
     markers.forEach((marker) => {
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup)
         .addTo(map);// add this
     });
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
     map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
   }
-
-  if (mapElement) {
-  // [...]
-    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken }));
-  }
-
-
 };
 
 
