0590070aff43f4c06ebe6ce4d3e44607bf68380e
gary weimberg <gary1992@hotmail.com>
2019-06-13 12:09:46 +0200

    fix issue

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -11,10 +11,6 @@ class Estimate < ApplicationRecord
     p = 1 - (1 + (rate / 12))**-years
     return (m / p)
   end
-
-  def spaces_on(attribute)
-    attribute.to_s.gsub(/\D/, '').reverse.gsub(/.{3}/, '\0 ').reverse
-  end
 end
 # estimate = project.estimates.last
 # estimate.spaces_on(market_price)
