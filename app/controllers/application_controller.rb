38bea6a3d6d0ce6dbe8ac7a2ae67e34dd7e472da
gary weimberg <gary1992@hotmail.com>
2019-06-13 16:55:11 +0200

    metastuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,4 +28,7 @@ class ApplicationController < ActionController::Base
   # def after_sign_in_path_for(resource)
   # request.env['omniauth.origin'] || stored_location_for(resource) || root_url
   # end
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
